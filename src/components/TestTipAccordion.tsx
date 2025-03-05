89ccabb8bcc372c5a8d53b8cb61326a0a0818efd
Wontae Chang <cwt629@naver.com>
2025-03-06 00:30:32 +0900

    design: [250306] 테스트 팁 accordion의 내부 내용을 내부 accordion으로 분리

--- src/components/TestTipAccordion.tsx
+++ src/components/TestTipAccordion.tsx
@@ -14,43 +14,74 @@ const TestTipAccordion = () => {
       <AccordionSummary expandIcon={<ExpandMore />}>
         <Box sx={{ display: "flex", alignItems: "center", gap: "12px" }}>
           <TipsAndUpdatesRounded />
-          <Typography>테스트 팁!</Typography>
+          <Typography>
+            <b>테스트 팁!</b>
+          </Typography>
         </Box>
       </AccordionSummary>
       <AccordionDetails>
-        <Alert severity="info">
-          <ul>
-            <li>
-              붉은색으로 칠해진 단어가 있으면, 그 <b>단어가 무조건 포함</b>
-              되어야 해요.
-            </li>
-            <li>
-              붉은색으로 칠해진 단어가 없으면, <b>내용이 정확히 일치</b>해야
-              해요.
-            </li>
-            <li>
-              <b>말씀 주소</b>는 띄어쓰기는 상관 없어요. 다만,{" "}
-              <b>물결표 대신 하이픈(-)</b>을 써주세요.
-              <br />
-              (ex. 창 1:1-2)
-            </li>
-            <li>
-              <s>개발자의 한계로</s> <b>구절 안에 적혀 있는 말씀 주소는</b>{" "}
-              띄어쓰기가 정확해야 해요.
-              <br />
-              <b>책이름과 절 사이만 띄어쓰기</b>가 있고, 그 외에는 띄어쓰기가
-              없어요.
-              <br />
-              (ex. 창 1:1-2 말씀시작)
-            </li>
-            <li>
-              구절 중에 <b>중략되어 있는 부분</b>은 양옆에 띄어쓰기 없이 ...을
-              붙여주세요.
-              <br />
-              (ex. 사람을 화나게...방법은 두 가지...첫째는...)
-            </li>
-          </ul>
-        </Alert>
+        <Accordion>
+          <AccordionSummary expandIcon={<ExpandMore />}>
+            필수 포함 단어
+          </AccordionSummary>
+          <AccordionDetails>
+            <Alert severity="info">
+              <ul>
+                <li>
+                  붉은색으로 칠해진 단어가 있으면, 그 <b>단어가 무조건 포함</b>
+                  되어야 해요.
+                </li>
+                <li>
+                  붉은색으로 칠해진 단어가 없으면, <b>내용이 정확히 일치</b>해야
+                  해요.
+                </li>
+              </ul>
+            </Alert>
+          </AccordionDetails>
+        </Accordion>
+        <Accordion>
+          <AccordionSummary expandIcon={<ExpandMore />}>
+            말씀 주소
+          </AccordionSummary>
+          <AccordionDetails>
+            <Alert severity="info">
+              <ul>
+                <li>
+                  <b>말씀 주소</b>는 띄어쓰기는 상관 없어요. 다만,{" "}
+                  <b>물결표 대신 하이픈(-)</b>을 써주세요.
+                  <br />
+                  (ex. 창 1:1-2)
+                </li>
+                <li>
+                  <s>개발자의 한계로</s> <b>구절 안에 적혀 있는 말씀 주소는</b>{" "}
+                  띄어쓰기가 정확해야 해요.
+                  <br />
+                  <b>책이름과 절 사이만 띄어쓰기</b>가 있고, 그 외에는
+                  띄어쓰기가 없어요.
+                  <br />
+                  (ex. 창 1:1-2 말씀시작)
+                </li>
+              </ul>
+            </Alert>
+          </AccordionDetails>
+        </Accordion>
+        <Accordion>
+          <AccordionSummary expandIcon={<ExpandMore />}>
+            말씀 중 중략된 부분
+          </AccordionSummary>
+          <AccordionDetails>
+            <Alert severity="info">
+              <ul>
+                <li>
+                  구절 중에 <b>중략되어 있는 부분</b>은 양옆에 띄어쓰기 없이
+                  ...을 붙여주세요.
+                  <br />
+                  (ex. 사람을 화나게...방법은 두 가지...첫째는...)
+                </li>
+              </ul>
+            </Alert>
+          </AccordionDetails>
+        </Accordion>
       </AccordionDetails>
     </Accordion>
   );
